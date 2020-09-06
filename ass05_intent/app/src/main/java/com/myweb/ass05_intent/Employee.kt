af1a9d862584b08082cbf921fcc20b41ac383ef3
Ponddd <69790350+Ponddd@users.noreply.github.com>
2020-09-07 02:05:29 +0700

    Initial commit

--- ass05_intent/app/src/main/java/com/myweb/ass05_intent/Employee.kt
+++
@@ -1,9 +0,0 @@
-package com.myweb.ass05_intent
-
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-data class Employee (val name: String, val gender:String,val email:String,val salary:Int) : Parcelable{
-
-}
